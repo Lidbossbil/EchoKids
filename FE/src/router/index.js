162d0921899d041f9f02d50e09cf2f84adf8b949
Nguyen Quoc Viet <nguyenqviet3885@gmail.com>
2026-04-24 22:40:18 +0700

    Bo sung beforeEnter

--- FE/src/router/index.js
+++ FE/src/router/index.js
@@ -41,51 +41,61 @@ const routes = [
     meta: { layout: "client" },
   },
 
+
   //---------------------------------------------KHACH HANG--------------------------------------------------------------
   {
     path: "/chu-de",
     component: () => import("../components/KhachHang/ChuDe/index.vue"),
     meta: { layout: "client" },
+    beforeEnter: checkClient,
   },
   {
     path: "/bai-hoc",
     component: () => import("../components/KhachHang/BaiHoc/index.vue"),
     meta: { layout: "client" },
+    beforeEnter: checkClient,
   },
   {
     path: "/chi-tiet-bai-hoc/:id",
     component: () => import("../components/KhachHang/ChiTietBaiHoc/index.vue"),
     meta: { layout: "client" },
+    beforeEnter: checkClient,
   },
   {
     path: "/bai-kiem-tra",
     component: () => import("../components/KhachHang/BaiKiemTra/index.vue"),
     meta: { layout: "client" },
+    beforeEnter: checkClient,
   },
   {
     path: "/luyen-tap",
     component: () => import("../components/KhachHang/LuyenTap/index.vue"),
     meta: { layout: "client" },
+    beforeEnter: checkClient,
   },
   {
     path: "/xep-hang",
     component: () => import("../components/KhachHang/XepHang/index.vue"),
     meta: { layout: "client" },
+    beforeEnter: checkClient,
   },
   {
     path: "/chuoi-ngay-hoc",
     component: () => import("../components/KhachHang/ChuoiNgayHoc/index.vue"),
     meta: { layout: "client" },
+    beforeEnter: checkClient,
   },
   {
     path: "/hoan-thanh-kiem-tra",
     component: () => import("../components/KhachHang/HoanThanhKT/index.vue"),
     meta: { layout: "client" },
+    beforeEnter: checkClient,
   },
   {
     path: "/tien-do",
     component: () => import("../components/KhachHang/TienDo/index.vue"),
     meta: { layout: "client" },
+    beforeEnter: checkClient,
   },
 
   //---------------------------------------------ADMIN--------------------------------------------------------------
@@ -93,38 +103,45 @@ const routes = [
     path: "/admin/dashboard",
     component: () => import("../components/AHeThong/Admin/Dashboard/index.vue"),
     meta: { layout: "admin" },
+    beforeEnter: checkAdmin,
   },
   {
     path: "/admin/quan-ly-tai-khoan",
     component: () =>
       import("../components/AHeThong/Admin/QuanLyTaiKhoan/index.vue"),
     meta: { layout: "admin" },
+    beforeEnter: checkAdmin,
   },
   {
     path: "/admin/kiem-duyet",
     component: () => import("../components/AHeThong/Admin/KiemDuyet/index.vue"),
     meta: { layout: "admin" },
+    beforeEnter: checkAdmin,
   },
   {
     path: "/admin/thong-ke",
     component: () => import("../components/AHeThong/Admin/ThongKe/index.vue"),
     meta: { layout: "admin" },
+    beforeEnter: checkAdmin,
   },
   {
     path: "/admin/phan-quyen",
     component: () => import("../components/AHeThong/Admin/PhanQuyen/index.vue"),
     meta: { layout: "admin" },
+    beforeEnter: checkAdmin,
   },
   {
     path: "/admin/quan-ly-cau-hinh",
     component: () =>
       import("../components/AHeThong/Admin/QuanLyCauHinhHeThong/index.vue"),
     meta: { layout: "admin" },
+    beforeEnter: checkAdmin,
   },
   {
     path: "/thong-tin-ca-nhan",
     component: () => import("../components/Client/Profile/index.vue"),
     meta: { layout: "admin" },
+    beforeEnter: checkAdmin,
   },
 
   //---------------------------------------------TEACHER--------------------------------------------------------------
@@ -132,28 +149,41 @@ const routes = [
     path: "/teacher/dashboard",
     component: () => import("../components/AHeThong/Teach/Dashboard/index.vue"),
     meta: { layout: "teach" },
+    beforeEnter: checkTeacher,
   },
   {
     path: "/teacher/quan-ly-hoc-sinh",
     component: () => import("../components/AHeThong/Teach/QuanLyHocSinh/index.vue"),
     meta: { layout: "teach" },
+    beforeEnter: checkTeacher,
   },
   {
     path: "/teacher/quan-ly-bai-hoc",
     component: () => import("../components/AHeThong/Teach/QuanLyBaiHoc/index.vue"),
     meta: { layout: "teach" },
+    beforeEnter: checkTeacher,
   },
   {
     path: "/teacher/bao-cao-thong-ke",
     component: () => import("../components/AHeThong/Teach/BaoCaoThongKe/index.vue"),
     meta: { layout: "teach" },
+    beforeEnter: checkTeacher,
   },
   {
     path: "/teacher/tu-vung",
     component: () => import("../components/AHeThong/Teach/TuVung/index.vue"),
     meta: { layout: "teach" },
+    beforeEnter: checkTeacher,
+  },
+    {
+    path: "/teacher/chat-box",
+    component: () => import("../components/AHeThong/Teach/ChatTeacher/index.vue"),
+    meta: { layout: "teach" },
+    beforeEnter: checkTeacher,
   },
 ];
+
+
 
 const router = createRouter({
   history: createWebHistory(),
